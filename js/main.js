8add8809ebd0ee2fadec7e721982d4cc65173dc7
Stephanie Fung <stephfung@Stephanies-MacBook-Pro.local>
2017-07-21 12:17:13 -0400

    adding to object to game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 // Testing our JS
 console.log("Up and running!");
 
-// Cards array, our empty selected cards array, and the cards that are selected
-var cards = ["queen", "queen", "king", "king"];
+// Our empty selected cards array for cards that are selected
 var cardsInPlay = [];
 
 //OLD CODE THAT WE DON'T NEED ANYMORE! Leaving it here for the moment until I have the new stuff written
 
 /*
+
+var cards = ["queen", "queen", "king", "king"];
+
 var cardOne = cards[0];
 var cardTwo = cards[2];
 
@@ -20,11 +22,34 @@ console.log("User flipped " + cardTwo);
 
 //The action that the cards will take when they are selected by the user
 
-
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+]
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
 		if (cardsInPlay.length === 2) {
 			if (cardsInPlay[0] === cardsInPlay[1]) {
 				alert("You found a match!");
